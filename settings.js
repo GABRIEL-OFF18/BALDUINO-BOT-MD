eb91f9a748c161fb490c4b79e3f06abc4a5eea72
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2025-12-03 15:19:10 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᴀʙʀᴀʜᴀɴ-ᴍ 𒆜"
 global.author = "© mᥲძᥱ ᥕі𝗍һ ᑲᥡ ᴀʙʀᴀʜᴀɴ-ᴍ 𒆜"
 global.etiqueta = "ᴀʙʀᴀʜᴀɴ-ᴍ"
 global.currency = "¥enes"
-global.banner = "https://files.catbox.moe/wak7xr.jpg"
+global.banner = "https://h.uguu.se/dhmlphFS.jpg"
 global.icono = "https://files.catbox.moe/boq8yt.jpg"
 global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
 
